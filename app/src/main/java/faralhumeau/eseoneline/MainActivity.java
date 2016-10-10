f1f58bb33e25e8bdb27ce9e33e5b597462f5f523
PatateDouce <helene.faral@gmail.com>
2016-10-10 09:38:04 +0200

    Hélène Test 2

--- app/src/main/java/faralhumeau/eseoneline/MainActivity.java
+++ app/src/main/java/faralhumeau/eseoneline/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //coucou les amis
+        //coucou les patates douces
         setContentView(R.layout.activity_main);
     }
 }
