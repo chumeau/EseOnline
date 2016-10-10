7ee0e451991ef2525ee1822fa876567a6eff44fc
chumeau <charline.humeau@reseau.eseo.fr>
2016-10-10 09:44:30 +0200

    Test conflit

--- app/src/main/java/faralhumeau/eseoneline/MainActivity.java
+++ app/src/main/java/faralhumeau/eseoneline/MainActivity.java
@@ -3,12 +3,13 @@ package faralhumeau.eseoneline;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //coucou les patates douces
+
         setContentView(R.layout.activity_main);
     }
 }
